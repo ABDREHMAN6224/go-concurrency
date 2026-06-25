ed24476ae5e8a7e5985500fd5479752196b68403
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-06-25 16:28:19 +0500

    for select pattern

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // sends nil channel in doWork which now remains in memory till process exits
 func GoRoutineLeak() {
@@ -20,6 +23,37 @@ func GoRoutineLeak() {
 	fmt.Println("Done")
 }
 
+func GoRoutineLeakFixed() {
+	doWork := func(done <-chan any, strings <-chan string) <-chan any {
+		compeleted := make(chan any)
+		go func() {
+			defer fmt.Println("doWork ended")
+			defer close(compeleted)
+			// it will be waiting here forever
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return compeleted
+	}
+	done := make(chan any)
+	terminated := doWork(done, nil)
+	go func() {
+		// Cancel the operation after 1 second.
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine...")
+		close(done)
+	}()
+	<-terminated
+	fmt.Println("Done.")
+}
+
 func main() {
-	GoRoutineLeak()
+	// GoRoutineLeak()
+	GoRoutineLeakFixed()
 }
