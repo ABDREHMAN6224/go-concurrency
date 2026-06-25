19e37edeeb4fd0b062e12f580ea1e9bd42489369
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-06-25 16:22:49 +0500

    leak

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+// sends nil channel in doWork which now remains in memory till process exits
+func GoRoutineLeak() {
+	doWork := func(strings <-chan string) <-chan any {
+		compeleted := make(chan any)
+		go func() {
+			defer fmt.Println("do work ended")
+			defer close(compeleted)
+			// it will be waiting here forever
+			for s := range strings {
+				fmt.Println(s)
+			}
+		}()
+		return compeleted
+	}
+	doWork(nil)
+	fmt.Println("Done")
+}
+
+func main() {
+	GoRoutineLeak()
 }
