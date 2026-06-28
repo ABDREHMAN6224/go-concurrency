1169ca72be9b4c5c4f51a8bf3c11c31670bada93
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-06-28 16:07:38 +0500

    healing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -35,8 +36,72 @@ func or(channels ...<-chan any) <-chan any {
 	return stream
 }
 
+func take(done <-chan any, stream <-chan any, nums int) <-chan any {
+	newStream := make(chan any)
+	go func() {
+		defer close(newStream)
+		for range nums {
+			select {
+			case <-done:
+				return
+			case newStream <- <-stream:
+			}
+		}
+	}()
+	return newStream
+}
+
 func main() {
 	type startGoroutineFn func(done <-chan any, pulseInterval time.Duration) (hearbeat <-chan any)
+	orDone := func(done <-chan any, stream <-chan any) <-chan any {
+		newStream := make(chan any)
+		go func() {
+			defer close(newStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-stream:
+					if ok == false {
+						return
+					}
+					select {
+					case <-done:
+						return
+					case newStream <- v:
+					}
+				}
+			}
+		}()
+		return newStream
+	}
+	bridge := func(done <-chan any, chanStream <-chan <-chan any) <-chan any {
+		valStream := make(chan any)
+		go func() {
+			defer close(valStream)
+			for {
+				var stream <-chan any
+				select {
+				case <-done:
+					return
+				case maybeStream, ok := <-chanStream:
+					if ok == false {
+						return
+					}
+					stream = maybeStream
+				}
+				for val := range orDone(done, stream) {
+					select {
+					case valStream <- val:
+					case <-done:
+						return
+
+					}
+				}
+			}
+		}()
+		return valStream
+	}
 
 	newSteward := func(
 		timeout time.Duration,
@@ -82,21 +147,58 @@ func main() {
 	}
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ltime | log.LUTC)
-	doWork := func(done <-chan any, _ time.Duration) <-chan any {
-		log.Println("ward: Hello, I'm irresponsible!")
-		go func() {
-			<-done
-			log.Println("ward: I am halting.")
-		}()
-		return nil
+
+	doWorkFn := func(done <-chan any, intList ...int) (startGoroutineFn, <-chan any) {
+		intChanStream := make(chan (<-chan any))
+		intStream := bridge(done, intChanStream)
+		doWork := func(
+			done <-chan any,
+			pulseInterval time.Duration,
+		) <-chan any {
+			intStream := make(chan any)
+			heartbeat := make(chan any)
+			go func() {
+				defer close(intStream)
+				select {
+				case intChanStream <- intStream:
+				case <-done:
+					return
+				}
+				pulse := time.Tick(pulseInterval)
+				for {
+				valueLoop:
+					for _, intVal := range intList {
+						if intVal < 0 {
+							log.Printf("negative value: %v\n", intVal)
+							return
+						}
+						for {
+							select {
+							case <-pulse:
+								select {
+								case heartbeat <- struct{}{}:
+								default:
+								}
+							case intStream <- intVal:
+								continue valueLoop
+							case <-done:
+								return
+							}
+						}
+					}
+				}
+			}()
+			return heartbeat
+		}
+		return doWork, intStream
 	}
-	doWorkWithSteward := newSteward(4*time.Second, doWork)
+
 	done := make(chan any)
-	time.AfterFunc(9*time.Second, func() {
-		log.Println("main: halting steward and ward.")
-		close(done)
-	})
-	for range doWorkWithSteward(done, 4*time.Second) {
+	defer close(done)
+	doWork, intStream := doWorkFn(done, 1, 2, -1, 3, 4, 5)
+	doWorkWithSteward := newSteward(1*time.Millisecond, doWork)
+	doWorkWithSteward(done, 1*time.Hour)
+	for intVal := range take(done, intStream, 6) {
+		fmt.Printf("Received: %v\n", intVal)
 	}
-	log.Println("Done")
 }
