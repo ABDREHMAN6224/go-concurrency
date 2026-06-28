f824e9d27d23c4740aa8dacc830297aa9740d5f5
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-06-28 15:38:56 +0500

    auto healing

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
+)
 
 func or(channels ...<-chan any) <-chan any {
 	switch len(channels) {
@@ -10,6 +14,23 @@ func or(channels ...<-chan any) <-chan any {
 		return channels[0]
 	}
 	stream := make(chan any)
+	go func() {
+		defer close(stream)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], stream)...):
+			}
+		}
+	}()
 
 	return stream
 }
@@ -25,16 +46,57 @@ func main() {
 			heartbeat := make(chan any)
 			go func() {
 				defer close(heartbeat)
-				var wardDone chan interface{}
-				var wardHeartbeat <-chan interface{}
+				var wardDone chan any
+				var wardHeartbeat <-chan any
 				startWard := func() {
-					wardDone = make(chan interface{})
+					wardDone = make(chan any)
 					wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
 				}
 				startWard()
 				pulse := time.Tick(pulseInterval)
+			monitorLoop:
+				for {
+					timeoutSignal := time.After(timeout)
+					for {
+						select {
+						case <-pulse:
+							select {
+							case heartbeat <- struct{}{}:
+							default:
+							}
+						case <-wardHeartbeat:
+							continue monitorLoop
+						case <-timeoutSignal:
+							log.Println("steward: ward unhealthy; restarting")
+							close(wardDone)
+							startWard()
+							continue monitorLoop
+						case <-done:
+							return
+						}
+					}
+				}
 			}()
 			return heartbeat
 		}
 	}
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+	doWork := func(done <-chan any, _ time.Duration) <-chan any {
+		log.Println("ward: Hello, I'm irresponsible!")
+		go func() {
+			<-done
+			log.Println("ward: I am halting.")
+		}()
+		return nil
+	}
+	doWorkWithSteward := newSteward(4*time.Second, doWork)
+	done := make(chan any)
+	time.AfterFunc(9*time.Second, func() {
+		log.Println("main: halting steward and ward.")
+		close(done)
+	})
+	for range doWorkWithSteward(done, 4*time.Second) {
+	}
+	log.Println("Done")
 }
